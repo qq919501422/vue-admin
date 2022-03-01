34c4831fca2402084084ebbc1ca4d500e03cdc32
Lin <919501422@qq.com>
2022-03-01 11:27:26 +0800

    构建项目

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-row>
+    <el-button color="#626aef" style="color: white">Custom</el-button>
+    <el-button color="#626aef" plain>Custom</el-button>
+  </el-row>
+</template>
+
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: "HomeView",
+  components: {
+
+  },
+};
+</script>
+<style scoped>
+
+</style>
